828837d23469f550b2c088ba433c1d61e1c7eefc
Tamás Zentai <zentaitamas@gmail.com>
2023-07-09 19:43:19 +0100

    add registration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css';
-
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
+
+
 
 app.mount('#app')
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,4 +1,25 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import {useRouter} from "vue-router";
+
+
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+
+const register = async () => {
+  try {
+    await createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+    console.log("User created")
+    router.push("/dashboard")
+
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+  }
+}
 
 </script>
 <template>
@@ -9,11 +30,12 @@
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+      <form class="space-y-6" @submit.prevent="register">
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
+            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required=""
+                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"/>
           </div>
         </div>
 
@@ -22,17 +44,22 @@
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
           <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
+            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required=""
+                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"/>
           </div>
         </div>
 
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-green-500 text-black px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-300">Create account</button>
+          <button type="submit"
+                  class="flex w-full justify-center rounded-md bg-green-500 text-black px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-300">
+            Create account
+          </button>
         </div>
       </form>
 
       <p class="mt-10 text-center text-sm text-gray-500">
-        <RouterLink to="/" class="font-semibold leading-6 text-green-500  hover:text-black">Back to login page</RouterLink>
+        <RouterLink to="/" class="font-semibold leading-6 text-green-500  hover:text-black">Back to login page
+        </RouterLink>
       </p>
     </div>
   </div>
