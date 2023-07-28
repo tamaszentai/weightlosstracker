753aefeedf493309d22f50dcd3a1a868abc4fa03
Tamas Zentai <zentaitamas@gmail.com>
2023-07-28 11:59:30 +0100

    modify and fixed animation in WeekItem component

--- src/components/WeekItem.vue
+++ src/components/WeekItem.vue
@@ -13,7 +13,7 @@ const weekToggle = () => {
 <template>
   <div class="mt-2 mb-2 grid grid-cols-1 sm:grid-cols-2">
     <div
-        :class="isOpen ? 'bg-green-200 relative flex flex-row-reverse items-center space-x-3 rounded-t-xl border-t border-r border-l border-gray-300 px-6 py-5 cursor-pointer' : 'bg-green-200 shadow-md relative flex flex-row-reverse items-center space-x-3 rounded-xl border border-gray-300  px-6 py-5 focus-within:ring-2 cursor-pointer'"
+        :class="isOpen ? 'overflow-hidden week-expanded bg-green-200 relative flex flex-row-reverse space-x-3 rounded-xl border border-gray-300 px-6 py-5 shadow-md cursor-pointer' : 'week overflow-hidden bg-green-200 shadow-md relative flex flex-row-reverse space-x-3 rounded-xl border border-gray-300 px-6 py-5 focus-within:ring-2 cursor-pointer'"
         @click="weekToggle">
       <ChevronUpIcon v-if="isOpen" class="w-10 h-10 rounded-full p-1 bg-gray-100"/>
       <ChevronDownIcon v-else class="w-10 h-10 rounded-full p-1 bg-gray-100"/>
@@ -21,56 +21,36 @@ const weekToggle = () => {
         <span class="absolute inset-0" aria-hidden="true"/>
         <p class="text-sm font-medium text-gray-900">{{ weekData.year }} - Week {{ weekData.weekNumber }}</p>
         <p class="truncate text-sm text-gray-500">Weekly average: {{ weekData.weeklyAverage }}kg</p>
+          <div class="my-4">
+              <p class="text-sm font-medium text-gray-900">Week start: <span
+                      class="text-sm text-gray-500">{{ weekData.weekStartDate.toDate().toDateString() }}</span></p>
+              <p class="text-sm font-medium text-gray-900">Week end: <span
+                      class="text-sm text-gray-500">{{ weekData.weekEndDate.toDate().toDateString() }}</span></p>
+              <br/>
+              <table class="min-w-full divide-y divide-gray-300">
+                  <tbody class="divide-y divide-gray-200">
+                  <tr v-for="day in weekData.days" :key="day.date">
+                      <td><span class="text-sm font-medium text-gray-900">{{ day.date.toDate().toDateString() }}</span></td>
+                      <td v-if="typeof day.weight !== 'string'" class="text-right"><span class="text-sm text-gray-500">{{ day.weight }}kg</span></td>
+                  </tr>
+                  </tbody>
+              </table>
+          </div>
       </div>
     </div>
-    <TransitionGroup name="expand">
-      <div v-if="isOpen"
-           class="expanded bg-green-200 rounded-b-xl border-b border-r border-l border-gray-300 shadow-md">
-        <div class="p-4">
-          <p class="text-sm font-medium text-gray-900">Week start: <span
-              class="text-sm text-gray-500">{{ weekData.weekStartDate.toDate().toDateString() }}</span></p>
-          <p class="text-sm font-medium text-gray-900">Week end: <span
-              class="text-sm text-gray-500">{{ weekData.weekEndDate.toDate().toDateString() }}</span></p>
-          <br/>
-          <table class="min-w-full divide-y divide-gray-300">
-<!--            <thead class="bg-gray-50">-->
-<!--            <tr>-->
-<!--              <th class="font-medium text-gray-500">-->
-<!--                Day-->
-<!--              </th>-->
-<!--              <th class="font-medium text-gray-500">-->
-<!--                Weight-->
-<!--              </th>-->
-<!--            </tr>-->
-<!--            </thead>-->
-            <tbody class="divide-y divide-gray-200">
-            <tr v-for="day in weekData.days" :key="day.date">
-              <td class="text-center"><span class="text-sm text-gray-500">{{ day.date.toDate().toDateString() }}</span></td>
-              <td v-if="typeof day.weight !== 'string'" class="text-center"><span class="text-sm text-gray-500">{{ day.weight }}kg</span></td>
-            </tr>
-            </tbody>
 
-          </table>
-        </div>
-      </div>
-    </TransitionGroup>
   </div>
 </template>
 
 <style scoped>
-.expanded {
-  height: 300px;
-  overflow: hidden;
+.week {
+    height: 5rem;
+    transition: .6s ease;
 }
 
-.expand-enter-active,
-.expand-leave-active {
-  transition: height 0.5s;
+.week-expanded {
+    height: 22rem;
+    transition: .6s ease;
 }
 
-.expand-enter,
-.expand-leave-to {
-  height: 0;
-  overflow: hidden;
-}
 </style>
